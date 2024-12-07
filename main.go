27c976627d2d6844c9520ad0977f58af20780e68
Kyle Reynolds <kylereynoldsdev@gmail.com>
2024-12-06 20:28:53 -0700

    day 4

--- main.go
+++ main.go
@@ -4,78 +4,48 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"regexp"
-	"strconv"
 )
 
 func main() {
+	// Read input into a 2D grid
+	var grid []string
 	scanner := bufio.NewScanner(os.Stdin)
-	sum := 0
-	enabled := true // Start with multiplications enabled
-	
-	// Regex patterns
-	mulRe := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
-	doRe := regexp.MustCompile(`do\(\)`)
-	dontRe := regexp.MustCompile(`don't\(\)`)
-	
 	for scanner.Scan() {
-		line := scanner.Text()
-		pos := 0
-		
-		for pos < len(line) {
-			// Find next occurrence of each pattern
-			mulMatch := mulRe.FindStringSubmatch(line[pos:])
-			mulIndex := mulRe.FindStringIndex(line[pos:])
-			doMatch := doRe.FindStringIndex(line[pos:])
-			dontMatch := dontRe.FindStringIndex(line[pos:])
-			
-			// Adjust matches to account for current position
-			if mulIndex != nil {
-				mulIndex[0] += pos
-				mulIndex[1] += pos
-			}
-			if doMatch != nil {
-				doMatch[0] += pos
-				doMatch[1] += pos
-			}
-			if dontMatch != nil {
-				dontMatch[0] += pos
-				dontMatch[1] += pos
-			}
-			
-			// Find the earliest match
-			nextPos := len(line)
-			if mulIndex != nil {
-				nextPos = mulIndex[0]
-			}
-			if doMatch != nil && doMatch[0] < nextPos {
-				nextPos = doMatch[0]
-			}
-			if dontMatch != nil && dontMatch[0] < nextPos {
-				nextPos = dontMatch[0]
-			}
-			
-			// Process the earliest match
-			if nextPos == len(line) {
-				break
-			}
-			
-			if doMatch != nil && doMatch[0] == nextPos {
-				enabled = true
-				pos = doMatch[1]
-			} else if dontMatch != nil && dontMatch[0] == nextPos {
-				enabled = false
-				pos = dontMatch[1]
-			} else if mulIndex != nil && mulIndex[0] == nextPos && mulMatch != nil {
-				if enabled {
-					x, _ := strconv.Atoi(mulMatch[1])
-					y, _ := strconv.Atoi(mulMatch[2])
-					sum += x * y
+		grid = append(grid, scanner.Text())
+	}
+	
+	rows := len(grid)
+	if rows == 0 {
+		return
+	}
+	cols := len(grid[0])
+	
+	count := 0
+	
+	// Check each possible center point
+	for i := 1; i < rows-1; i++ {
+		for j := 1; j < cols-1; j++ {
+			if grid[i][j] == 'A' {
+				// Check all possible combinations of MAS in X pattern
+				// Upper-left to lower-right and upper-right to lower-left
+				if isValidXMAS(grid, i, j, rows, cols) {
+					count++
 				}
-				pos = mulIndex[1]
 			}
 		}
 	}
 	
-	fmt.Println(sum)
+	fmt.Println(count)
+}
+
+func isValidXMAS(grid []string, i, j, rows, cols int) bool {
+	// Check if we can form valid MAS strings in both diagonals
+	ul := string([]byte{grid[i-1][j-1], grid[i][j], grid[i+1][j+1]})  // upper-left to lower-right
+	ur := string([]byte{grid[i-1][j+1], grid[i][j], grid[i+1][j-1]})  // upper-right to lower-left
+	
+	return isValidArm(ul) && isValidArm(ur)
+}
+
+func isValidArm(s string) bool {
+	return s == "MAS" || s == "SAM"
 }
